c9f9bd9a4a66db6ba292be8068ad974538f23a7b
Vishal Sahgal <mrvishalsahgal@gmail.com>
2026-03-21 10:32:16 +0530

    feat: Implement Redux for user state management, add login functionality, and configure application routing.

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,24 @@ import Body from "./Body";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Profile from "./Profile";
 import Login from "./Login";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Feed from "./feed";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/login" element={<Login />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/feed" element={<Feed />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "./utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "./utils/constant";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("aryadi@gmail.com");
+  const [password, setPassword] = useState("Arya@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        BASE_URL + "/login",
+        {
+          email: email,
+          password: password,
+        },
+        {
+          withCredentials: true,
+        },
+      );
+      dispatch(addUser(response.data));
+      return navigate("/feed");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center mt-[10%]">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border  p-4">
+        <legend className="fieldset-legend">Login</legend>
+
+        <label className="label">Email</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="input"
+          placeholder="Email"
+        />
+
+        <label className="label">Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="input"
+          placeholder="Password"
+        />
+
+        <button className="btn btn-neutral mt-4" onClick={handleLogin}>
+          Login
+        </button>
+      </fieldset>
+    </div>
+  );
 };
 
 export default Login;
